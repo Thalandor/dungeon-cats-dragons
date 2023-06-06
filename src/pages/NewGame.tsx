1f05f58f90becd035339b4abb89319381bcb8690
Ivan Bargallo <ivan.bargallo@betterask.erni>
2023-06-06 23:24:04 +0200

    Added dnd font and key hook

--- src/pages/NewGame.tsx
+++ src/pages/NewGame.tsx
@@ -1,6 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import Character from "../components/character/Character";
+import { useKeyPress } from "../hooks/useKeyPress";
 import styles from "./NewGame.module.scss";
+
 const NewGame = () => {
+  const navigate = useNavigate();
+  useKeyPress(() => navigate("/"), ["Escape"]);
   return (
     <div className={styles.parent}>
       <div style={{ flex: 80 }}>
