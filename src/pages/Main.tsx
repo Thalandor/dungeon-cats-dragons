1976f5d4050e068a77ace06274892632a56f6385
Ivan Bargallo <ivan.bargallo@betterask.erni>
2023-06-08 01:05:55 +0200

    Main page no web3

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,21 +1,24 @@
-import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import styles from "./Main.module.scss";
+import yousad from "../assets/yousad.png";
 const Main = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/new">New Game</Link>
-          </li>
-          <li>
-            <Link to="/market">Market</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className={styles.parent}>
+      <div className={styles.title}>
+        <img className={styles.rotate} src={yousad}></img>
+        <div className={styles.and}>&</div>
+        <div className={styles.elements}>
+          <div>Dungeons</div>
+          <div>Cats</div>
+          <div>Dragons</div>
+        </div>
+        <img src={yousad}></img>
+      </div>
+      <div className={styles.options}>
+        <div onClick={() => navigate("/new")}>New game</div>
+        <div onClick={() => navigate("/market")}>Market</div>
+      </div>
     </div>
   );
 };
