6cf8faa166ba9ff46b0d6e40025a740410c3653d
Ivan Bargallo <3891584+Thalandor@users.noreply.github.com>
2023-06-12 20:29:29 +0200

    Add tostify container

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from "./App.tsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { Web3Provider } from "./providers/Web3.tsx";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Web3Provider>
         <App />
+        <ToastContainer />
       </Web3Provider>
     </BrowserRouter>
   </React.StrictMode>
